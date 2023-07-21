ec97cdd2760231f69a80e7c3d6957ee3058cd94b
Joshua_Le_Roux <joshualeroux.jlr@gmail.com>
2023-07-21 16:12:42 +0200

    added fetch

--- src/App.js
+++ src/App.js
@@ -6,10 +6,24 @@ import { useState } from 'react';
 import Buttons from "./buttons"
 
 function App() {
+  const [weather, setWeather] = useState(null)
   //useState to access the user input and assign it to the state
   const [addTodo, setaddTodo] = useState('')
   //calling dispatch
   const dispatch = useDispatch();
+
+  fetch("https://api.open-meteo.com/v1/forecast?latitude=-33.9258&longitude=18.4232&hourly=temperature_2m,relativehumidity_2m,windspeed_10m&current_weather=true")
+  //converting from text to obj
+  .then(res => res.json())
+  .then((result)=> {
+      //grabbing the current temp in
+      let currentTemp = result.current_weather.temperature
+          // returning current temp in Paris
+      setWeather(currentTemp)
+  }),
+  (error) => {
+      console.log(error)
+  }
 
   const submitAdd = (event) => {
     event.preventDefault();
@@ -24,6 +38,7 @@ function App() {
 
   return (
     <div className="App">
+      {weather && <h2>Cape Town : {weather} C</h2>}
       <form>
         <label>Add Task<br/>  
         {/* when input field is changed its change is monitored and then set to the useState */}
